e098be8cf5ebaa37536a5b4a89ef5424b8b5c01f
kiclu <kiclu@protonmail.com>
2022-06-25 17:27:48 +0200

    semaphore revision

--- src/sem.c
+++ src/sem.c
@@ -4,37 +4,13 @@
 #include"../h/sched.h"
 #include"../h/thread.h"
 #include"../h/trap.h"
+#include"../h/syscall_c.h"
 
 #define SEM_SIZE sizeof(_sem)
 
 static void panic(){ for(;;); }
 
 extern void queue_waiting_push(thread_t);
-
-int __sem_open(sem_t* handle, uint value){
-    if(handle == NULL) panic();
-
-    *handle = __kmem_alloc(SEM_SIZE);
-    if(*handle == NULL) return -1;
-
-    (*handle)->value = (int)value;
-    (*handle)->blocked_queue_front = (*handle)->blocked_queue_back = NULL;
-    return 0;
-}
-
-int __sem_close(sem_t handle){
-    if(handle == NULL) return -1;
-
-    // check if any threads are still waiting on this semaphore
-    if(handle->blocked_queue_front != NULL || handle->blocked_queue_back != NULL){
-        return -1;
-    }
-
-    // no threads are waiting, delete semaphore
-    __kmem_free(handle);
-    handle = NULL;
-    return 0;
-}
 
 static void queue_sem_push(thread_t thr, sem_t id){
     thr->state = BLOCKED;
@@ -70,32 +46,54 @@ static void yield_sem(thread_t new, sem_t id){
     running->cpu_time = 0;
 }
 
+int __sem_open(sem_t* handle, uint value){
+    if(handle == NULL) panic();
+
+    *handle = __kmem_alloc(SEM_SIZE);
+    if(*handle == NULL) return -1;
+
+    (*handle)->value = (int)value;
+    (*handle)->blocked_queue_front = (*handle)->blocked_queue_back = NULL;
+    return 0;
+}
+
+int __sem_close(sem_t handle){
+    if(handle == NULL) return -1;
+
+    // unblock any threads waiting on this semaphore
+    thread_t blocked = NULL;
+    while((blocked = queue_sem_pop(handle)) != NULL){
+        queue_waiting_push(blocked);
+    }
+
+    // no threads are waiting, delete semaphore
+    __kmem_free(handle);
+    handle = NULL;
+    return 0;
+}
+
 int __sem_wait(sem_t id){
     if(id == NULL) return -1;
 
-    if(id->value > 0){
-        --id->value;
-        return 0;
-    }
+    // if value greater than 0, decrement and return immediately
+    if(id->value > 0){ --id->value; return 0; }
 
+    // else block thread and yield
     thread_t new = NULL;
-    while((new = sched()) == NULL){
-        if(id->value > 0){ --id->value; return 0; }
-    }
+    while((new = sched()) == NULL);
     yield_sem(new, id);
-
-    --id->value;
 
     return 0;
 }
 
 int __sem_signal(sem_t id){
     if(id == NULL) return -1;
-
-    ++id->value;
     
+    // if there are threads waiting, unblock one and return
+    // else increment value and return
     thread_t blocked = queue_sem_pop(id);
-    if(blocked != NULL) queue_waiting_push(blocked);
+    if(blocked == NULL) ++id->value;
+    else queue_waiting_push(blocked);
 
     return 0;
 }
